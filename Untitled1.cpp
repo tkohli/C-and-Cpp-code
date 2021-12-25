96256a1ba864a55b49c910d0737ddb43bbff2fb7
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-25 13:34:12 +0530

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main(){
-	printf("Hello world");
+	printf("Hello\nworld");
+	printf("Hello\tworld");
+	printf("Hello\aworld");
+	printf("Hello\rworld");
+	printf("Hello\bworld");
 	return 0;
 }
